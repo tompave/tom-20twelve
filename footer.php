b9b4a1e7129f792920a3c03537ad8a3c99457857
tom <git@wonderingmachine.com>
2012-12-27 19:31:32 +0100

    added link to repo in the footer, removed functions.php file

--- footer.php
+++ footer.php
@@ -13,7 +13,7 @@
 	</div><!-- #main .wrapper -->
 	<footer id="colophon" role="contentinfo">
 		<div class="site-info">
-			Powered by <a href="http://wordpress.org/">WordPress</a>. Custom theme based on <a href="http://wordpress.org/extend/themes/twentytwelve">Twenty Twelve</a>.
+			Powered by <a href="http://wordpress.org/">WordPress</a>. <a href="https://github.com/tompave/tom-20twelve">Custom theme</a> based on <a href="http://wordpress.org/extend/themes/twentytwelve">Twenty Twelve</a>.
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
